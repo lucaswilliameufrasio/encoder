7e2d1775725a7029ee27ee686b6c3892f67d614e
Lucas William <lucaswilliameufrasio@gmail.com>
2021-05-28 11:28:44 -0300

    chore: add explanation

--- src/encoder.js
+++ src/encoder.js
@@ -8,12 +8,17 @@ module.exports = class Encoder {
 
         if (encodedNumber.length < 6) {
             const missingCharacters = this.codeLength - encodedNumber.length
+
+            // Concatenate N times the special character
             encodedNumber += this.specialCharacter.repeat(missingCharacters).toString()
         }
 
         if (encodedNumber.length > 6) {
             const usablePortion = encodedNumber.substring(0, 5)
+
+            // Get the sixth position digit and convert to decimal
             const decimalOfTheDigitAtSixthPosition = parseInt(encodedNumber.substring(5, 6), 16)
+            // Get a substitute on the dictionary to the sixth position character
             const characterSubstitute = this.dictionary[decimalOfTheDigitAtSixthPosition]
             
             encodedNumber = usablePortion.toString() + characterSubstitute.toString()
@@ -40,6 +45,8 @@ module.exports = class Encoder {
 
     decode(code) {
         let decoded = 0
+        
+        // Removes the special character
         const codeWithoutSpecialCharacter = code.replace(`${this.specialCharacter}`, '')
 
         const foundDictionaryCharacterPosition = this.findDictionaryCharacterPositionOnCode(codeWithoutSpecialCharacter)
@@ -48,8 +55,11 @@ module.exports = class Encoder {
             const usablePortion = codeWithoutSpecialCharacter.substring(0, 5)
             
             for (let index = 5; index > 0; index--) {
+                // Reverse the string and extract a character from the code
                 const character = usablePortion.split('').reverse().join('').substring(index - 1, index);
 
+                // Convert each character to decimal and multiply per 16 by power of a number,
+                // Where this number is the current index.
                 decoded += this.convertValueFromHexadecimalToDecimal(parseInt(character, 16), index + 1)
             }
             
