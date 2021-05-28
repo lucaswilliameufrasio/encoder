3e5aeacc05a262fbac924b9c5bcff02d631c0025
Lucas William <lucaswilliameufrasio@gmail.com>
2021-05-28 10:17:46 -0300

    test: ensure Encoder returns a code with fixed length also with a two digit number

--- test/encoder.spec.js
+++ test/encoder.spec.js
@@ -3,10 +3,13 @@ const Encoder = require('../src/encoder')
 describe('Encoder', () => {
   it('Should return a code with fixed length of 6', () => {
     const numberWithOneDigit = 9
+    const numberWithTwoDigits = 12
     const sut = new Encoder()
 
-    const code = sut.encode(numberWithOneDigit)
+    const code1 = sut.encode(numberWithOneDigit)
+    const code2 = sut.encode(numberWithTwoDigits)
     
-    expect(code).toHaveLength(6)
+    expect(code1).toHaveLength(6)
+    expect(code2).toHaveLength(6)
   })
 })
