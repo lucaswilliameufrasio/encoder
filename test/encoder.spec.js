60dd5d30bc5ffea737ac24e9a4a0e48a0546b2eb
Lucas William <lucaswilliameufrasio@gmail.com>
2021-05-28 10:19:20 -0300

    test: ensure Encoder returns a code with fixed length also with a four digit number

--- test/encoder.spec.js
+++ test/encoder.spec.js
@@ -5,14 +5,17 @@ describe('Encoder', () => {
     const numberWithOneDigit = 9
     const numberWithTwoDigits = 12
     const numberWithThreeDigits = 123
+    const numberWithFourDigits = 1234
     const sut = new Encoder()
 
     const code1 = sut.encode(numberWithOneDigit)
     const code2 = sut.encode(numberWithTwoDigits)
     const code3 = sut.encode(numberWithThreeDigits)
+    const code4 = sut.encode(numberWithFourDigits)
     
     expect(code1).toHaveLength(6)
     expect(code2).toHaveLength(6)
     expect(code3).toHaveLength(6)
+    expect(code4).toHaveLength(6)
   })
 })
