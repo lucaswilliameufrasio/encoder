99dab3ea28a2795fab9ef5083ce2d830aca7e37a
Lucas William <lucaswilliameufrasio@gmail.com>
2021-05-28 10:18:27 -0300

    test: ensure Encoder returns a code with fixed length also with a three digit number

--- test/encoder.spec.js
+++ test/encoder.spec.js
@@ -4,12 +4,15 @@ describe('Encoder', () => {
   it('Should return a code with fixed length of 6', () => {
     const numberWithOneDigit = 9
     const numberWithTwoDigits = 12
+    const numberWithThreeDigits = 123
     const sut = new Encoder()
 
     const code1 = sut.encode(numberWithOneDigit)
     const code2 = sut.encode(numberWithTwoDigits)
+    const code3 = sut.encode(numberWithThreeDigits)
     
     expect(code1).toHaveLength(6)
     expect(code2).toHaveLength(6)
+    expect(code3).toHaveLength(6)
   })
 })
