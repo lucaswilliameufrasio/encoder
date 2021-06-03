0f0020a9c9544f327f6ffdb8c350f7a94988dc2d
Lucas William <lucaswilliameufrasio@gmail.com>
2021-06-03 16:48:18 -0300

    style: lint

--- src/index.js
+++ src/index.js
@@ -1,27 +1,27 @@
-const Encoder = require('./encoder')
+const Encoder = require("./encoder");
 
-const repl = require('repl')
+const repl = require("repl");
 
-const encoder = new Encoder()
+const encoder = new Encoder();
 
-const replServer = repl.start({ prompt: '> ' })
+const replServer = repl.start({ prompt: "> " });
 
-replServer.defineCommand('encode', {
-  help: 'Encode number',
+replServer.defineCommand("encode", {
+  help: "Encode number",
   action(number) {
-    const code = encoder.encode(number)
-    this.clearBufferedCommand()
-    console.log(code)
-    this.displayPrompt(code)
+    const code = encoder.encode(number);
+    this.clearBufferedCommand();
+    console.log(code);
+    this.displayPrompt(code);
   },
-})
+});
 
-replServer.defineCommand('decode', {
-  help: 'Decode code',
+replServer.defineCommand("decode", {
+  help: "Decode code",
   action(code) {
-    const number = encoder.decode(code)
-    this.clearBufferedCommand()
-    console.log(number)
-    this.displayPrompt(number)
+    const number = encoder.decode(code);
+    this.clearBufferedCommand();
+    console.log(number);
+    this.displayPrompt(number);
   },
-})
+});
